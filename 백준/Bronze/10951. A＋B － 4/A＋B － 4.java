971594b747e392c7509ff1de7cde692b4310e327
HanaKong <wonbin1301@gmail.com>
2023-12-09 21:54:22 +0900

    [Bronze V] Title: A+B - 4, Time: 232 ms, Memory: 17852 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        while (scanner.hasNextInt()) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            
+            int C = A + B;
+            
+            System.out.println(C);
+        }
+        scanner.close();
+    }
+}
