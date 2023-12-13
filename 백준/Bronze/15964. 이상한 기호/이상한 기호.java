90dd56758bdb265129f6d62005081f2847a9631c
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2023-12-13 23:57:05 +0900

    [Bronze V] Title: 이상한 기호, Time: 208 ms, Memory: 17644 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int result = 0;
+        
+        scanner.close();
+        
+        result = (A + B) * (A - B);
+        
+        System.out.println(result);
+    }
+}
