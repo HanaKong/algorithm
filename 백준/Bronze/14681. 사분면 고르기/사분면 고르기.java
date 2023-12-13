b4181668b71fb9c6a657ce370502ec12422ecadc
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2023-12-13 21:21:05 +0900

    [Bronze V] Title: 사분면 고르기, Time: 116 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int X = Integer.parseInt(br.readLine());
+        int Y = Integer.parseInt(br.readLine());
+        
+        if (X > 0) {
+            if (Y > 0) System.out.println(1);
+            else System.out.println(4); // 0은 제외이므로
+        }
+        else {
+            if (Y > 0) System.out.println(2);
+            else System.out.println(3);
+        }
+    }
+}
