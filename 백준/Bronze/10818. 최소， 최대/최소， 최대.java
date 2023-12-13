3d48f4f5f7c91419fe040249b2f957ca83e1e94d
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2023-12-13 23:36:17 +0900

    [Bronze III] Title: 최소, 최대, Time: 1748 ms, Memory: 319324 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int N = scanner.nextInt();
+        int arr[] = new int[N];
+
+        int max = -1000000;
+        int min = 1000000;
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println(min + " " + max);
+        
+        scanner.close();
+    }
+}
