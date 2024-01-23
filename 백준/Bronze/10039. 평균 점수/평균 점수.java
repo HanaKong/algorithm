6c3191b8e5db98f56b99f61cba0be49a8e5762ce
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2024-01-23 20:18:48 +0900

    [Bronze IV] Title: 평균 점수, Time: 208 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int temp = sc.nextInt();
+
+            if (temp < 40) {
+                sum += 40;
+            }
+            else {
+                sum += temp;
+            }
+        }
+        System.out.println(sum/5);
+    }
+}
