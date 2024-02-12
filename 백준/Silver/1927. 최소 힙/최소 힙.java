f167c13d40a4571a97d79e6f143bd32ce86abd81
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2024-02-12 09:58:04 +0900

    [Silver II] Title: 최소 힙, Time: 1772 ms, Memory: 115080 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,25 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		PriorityQueue<Integer> q = new PriorityQueue<>();
+		
+		for (int i = 0; i < N; i++) {
+			int A = sc.nextInt();
+			if (A == 0) {
+				if (q.isEmpty()) {
+					System.out.println("0");
+				}else {
+					System.out.println(q.poll());
+				}
+			}
+			else {
+				q.offer(A);
+			}
+		}
+	}
+}
