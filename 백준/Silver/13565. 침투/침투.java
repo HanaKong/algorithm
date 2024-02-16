20ddb598a1cb814b679b328e001f862e04cd8ffd
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2024-02-16 21:13:40 +0900

    [Silver II] Title: 침투, Time: 1592 ms, Memory: 56616 KB -BaekjoonHub

---
+++ 백준/Silver/13565. 침투/침투.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	final static int MAX = 1010;
+	static boolean[][] map;
+	static int dirY[] = {-1, 1, 0, 0};
+	static int dirX[] = {0, 0, -1, 1};
+	static int T, N, M, K;
+	static boolean answer;
+	
+	public static void dfs(int y, int x) {
+		if (y == N) {
+			answer = true;
+			return;
+		}
+		map[y][x] = false;
+		for (int i = 0; i < 4; i++) {
+			int newY = y + dirY[i];
+			int newX = x + dirX[i];
+			if (map[newY][newX]) {
+				dfs(newY, newX);
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new boolean[MAX][MAX];
+		
+		for (int i = 1; i <= N; i++) {
+			String str = br.readLine();
+			for (int j = 1; j <= M; j++) {
+				map[i][j] = (str.charAt(j - 1) == '0') ? true : false;
+			}
+		}
+		
+		for (int j = 1; j <= M; j++) {
+			if (map[1][j]) {
+				dfs(1, j);
+			}
+		}
+		if (answer) bw.write("YES");
+		else bw.write("NO");
+		
+		bw.close();
+		br.close();
+	}
+}
