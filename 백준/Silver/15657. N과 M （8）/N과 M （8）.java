22e0158ce2e9158624a9af8fe9f7c2760b4fcd71
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2024-03-03 09:27:29 +0900

    [Silver III] Title: N과 M (8), Time: 172 ms, Memory: 18764 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	static int N, M;
+	static int[] arr;
+	static int[] num;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		visited = new boolean[N];
+		arr = new int[M];
+		num = new int[N];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(num);
+		
+		dfs(0, 0);
+		
+		System.out.println(sb);
+		
+		br.close();
+	}
+
+	static void dfs(int depth, int start) {
+		if (depth == M) {
+			for (int i : arr) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = start; i < N; i++) {
+			if (!visited[i]) {
+				arr[depth] = num[i];
+				dfs (depth + 1, i);
+			}
+		}
+	}
+}
