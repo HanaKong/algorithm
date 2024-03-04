4147c3597eb22a1f68d5c3f9ac5ae0c51e62ab48
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2024-03-04 21:34:39 +0900

    [Gold IV] Title: 알파벳, Time: 1156 ms, Memory: 13484 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int R, C, max;
+	static int[] dirY = {-1, 1, 0, 0};
+	static int[] dirX = {0, 0, -1, 1};
+	static int[][] map;
+	static boolean[][] visited;
+	static boolean[] alphabet = new boolean[26];
+	
+	public static void dfs(int y, int x, int cnt) {
+		alphabet[map[y][x]] = true;
+		visited[y][x] = true;
+		
+		for (int i = 0; i < 4; i++) {
+			int newY = y + dirY[i];
+			int newX = x + dirX[i];
+			
+			if (0 <= newY && newY < R && 0 <= newX && newX < C && !visited[newY][newX] && !alphabet[map[newY][newX]]) {
+				dfs(newY, newX, cnt+1);
+			}
+		}
+		max = Math.max(max, cnt);
+		alphabet[map[y][x]] = false;
+		visited[y][x] = false;
+	}	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		map = new int[R][C];
+		visited = new boolean[R][C];
+		
+		for (int i = 0; i < R; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < C; j++) {
+				map[i][j] = str.charAt(j) - 'A';
+			}
+		}
+		
+		dfs(0, 0, 1);
+		
+		bw.write(String.valueOf(max));;
+		
+		bw.close();
+		br.close();
+	}
+}
