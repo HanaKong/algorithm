82a97032b642f1e014706672106c76b21037508e
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2024-03-20 14:39:55 +0900

    [Silver III] Title: 파도반 수열, Time: 80 ms, Memory: 11648 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,14 +1,18 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int T = sc.nextInt();
+		int T = Integer.parseInt(br.readLine());
 		
 		for (int tc = 0; tc < T; tc++) {
-			int N = sc.nextInt();
+			int N = Integer.parseInt(br.readLine());
 			long[] d = new long[N+1];
 			for (int i = 1; i < N + 1; i++) {
 				if (i < 4) {
@@ -21,7 +25,11 @@ public class Main {
 			for (int i = 4; i <= N; i++) {
 				d[i] = d[i - 2] + d[i - 3];
 			}
-			System.out.println(d[N]);
+			bw.write(String.valueOf(d[N]));
+			bw.newLine();
 		}
+		
+		bw.close();
+		br.close();
 	}
 }
