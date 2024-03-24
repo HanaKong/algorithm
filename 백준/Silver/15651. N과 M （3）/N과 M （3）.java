5b8d241abb54d1bc79d7fc95d7c005ca63380db7
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2024-03-25 06:37:41 +0900

    [Silver III] Title: N과 M (3), Time: 564 ms, Memory: 272028 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,40 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static int N;
-	public static int M;
-	public static int[] arr;
-	public static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		N = sc.nextInt();
+		M = sc.nextInt();
 		
 		arr = new int[M];
 		
-		permutation(0);
+		dfs(0);
+		
 		System.out.println(sb);
+				
 	}
-	public static void permutation(int cnt) {
-		if (cnt == M) {
-			for (int i = 0; i < arr.length; i++) {
-				sb.append(arr[i] + " ");
-				if ((i+1) % M == 0) {
-					sb.append("\n");
-				}
+	public static void dfs(int depth) {
+		if (depth == M) {
+			for (int i : arr) {
+				sb.append(i + " ");
 			}
+			sb.append("\n");
 			return;
 		}
+		
 		for (int i = 1; i <= N; i++) {
-				arr[cnt] = i;
-				permutation(cnt + 1);
+			arr[depth] = i;
+			dfs(depth + 1);
 		}
 	}
 }
