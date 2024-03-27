588ba78b93e5bfc7684e6da1b0ea7f70f6a9f065
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2024-03-27 16:28:40 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 580 ms, Memory: 56652 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -14,14 +14,10 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		int[] arr = new int[N+1];
 		int[] sum = new int[N+1];
 		st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		for (int i = 1; i <= N; i++) {
-			sum[i] = sum[i-1] + arr[i];
+			sum[i] = sum[i-1] + Integer.parseInt(st.nextToken());
 		}
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
