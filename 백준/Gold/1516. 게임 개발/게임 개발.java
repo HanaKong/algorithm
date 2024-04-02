0c371eb7742eda099c0bd1de1b025d2a260fa380
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2024-04-02 15:04:13 +0900

    [Gold III] Title: 게임 개발, Time: 284 ms, Memory: 19864 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -12,24 +11,18 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		Queue<int[]> q = new ArrayDeque<>();
+		Queue<Integer> q = new ArrayDeque<>();
 		
 		int N = Integer.parseInt(br.readLine());
-		// 시간
 		int[] cost = new int[N+1];
-		// 진입차수
 		int[] arr = new int[N+1];
-		
-		// 해당 번호 건물 걸리는 시간
 		int[] res = new int[N+1];
-		int[] temp = new int[N+1];
 		
 		List<Integer>[] list = new ArrayList[N + 1];
 		
 		for(int i = 0; i < N + 1; i++) {
 			list[i] = new ArrayList<>();
 		}
-		
 		
 		for(int i = 1; i < N + 1; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
@@ -44,40 +37,27 @@ public class Main {
 		
 		for(int i = 1; i < N + 1; i++) {
 			if (arr[i] == 0) {
-				q.offer(new int[] {i, cost[i]});
-				temp[i] = cost[i];
+				q.offer(i);
+				res[i] = cost[i];
 			}
 		}
 		
 		while(!q.isEmpty()) {
-			int[] d = q.poll();
+			int d = q.poll();
 			
-			int num = d[0];
-			int time = d[1];
-			
-			res[num] = time;
-			
-			for(int i = 0; i < list[num].size(); i++) {
-				arr[list[num].get(i)]--;
+			for(int i = 0; i < list[d].size(); i++) {
+				arr[list[d].get(i)]--;
 				
-				
-				// 다음 지점
-				temp[list[num].get(i)] = Math.max(temp[num]+cost[list[num].get(i)], temp[list[num].get(i)]);
-				
-//				System.out.println(num+" "+list[num].get(i));
-//				System.out.println(Arrays.toString(temp));
+				res[list[d].get(i)] = Math.max(res[d]+cost[list[d].get(i)], res[list[d].get(i)]);
 
-				if (arr[list[num].get(i)] == 0) {
-					q.offer(new int[] {list[num].get(i), time + cost[list[num].get(i)]});
+				if (arr[list[d].get(i)] == 0) {
+					q.offer(list[d].get(i));
 				}
 			}
 		}
-//		for (int i = 1; i < N + 1; i++) {
-//			temp[i] += cost[i];
-//			if (res[i] < temp[i]) res[i] = temp[i];
-//		}
+
 		for (int i = 1; i < N + 1; i++) {
-			System.out.println(temp[i]);
+			System.out.println(res[i]);
 		}
 	}
 }
