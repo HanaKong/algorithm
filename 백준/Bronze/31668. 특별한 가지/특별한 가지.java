c55dc3e74279ad7d518115f9f77d1f52e2d193d3
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2024-04-02 11:54:59 +0900

    [Bronze IV] Title: 특별한 가지, Time: 108 ms, Memory: 12828 KB -BaekjoonHub

--- 백준/Bronze/31668. 특별한 가지/특별한 가지.java
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.java
@@ -1,12 +1,1 @@
-import java.util.Scanner;
-
-public class Main {
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int N, M , K;
-		N = s.nextInt();
-		M = s.nextInt();
-		K = s.nextInt();
-		System.out.println(M/N*K);
-	}
-}
+import java.util.*;public class Main {public static void main(String[] args) {Scanner s = new Scanner(System.in);int n = s.nextInt();System.out.println(s.nextInt()/n*s.nextInt());}}
