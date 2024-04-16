8b6ad05f3025480316971ee52321386fb0d971e0
정원빈 (Wonbin Jeong) <wonbin1301@gmail.com>
2024-04-16 16:40:36 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1056 ms, Memory: 234064 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -11,24 +11,16 @@ public class Main {
 		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < N; i++) {
-			int tmp = 0;
 			int num = Integer.parseInt(st.nextToken());
-			if (nums.get(num) != null) {
-				tmp = nums.get(num);
-			}
-			nums.put(num, tmp + 1);
+            // getOrDefault = 값이 있을 때 없을 때 조건 추가
+			nums.put(num, nums.getOrDefault(num, 0) + 1);
 		}
 		int M = Integer.parseInt(br.readLine());
 		
 		st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < M; i++) {
 			int res = Integer.parseInt(st.nextToken());
-			if (nums.get(res) != null) {
-				sb.append(nums.get(res)).append(" ");
-			}
-			else {
-				sb.append(0).append(" ");
-			}
+			sb.append(nums.getOrDefault(res, 0)).append(" ");
 		}
 		
 		System.out.println(sb);
